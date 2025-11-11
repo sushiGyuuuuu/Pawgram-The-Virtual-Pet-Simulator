5bac533206af2fcdc6bf6f4ca27117094489ef19
Sushi Roll <gabb09.perez@gmail.com>
2025-11-11 20:04:57 +0800

    Add files via upload

---
+++ Pawgram The Virtual Pet Simulator/Pawgram/src/paw/managers/ShopManager.java
@@ -0,0 +1,9 @@
+package paw.managers;
+
+import paw.models.*;
+import java.util.*;
+
+public class ShopManager {
+    private List<Item> items = new ArrayList<>();
+    private Scanner input = new Scanner(System.in);
+}
