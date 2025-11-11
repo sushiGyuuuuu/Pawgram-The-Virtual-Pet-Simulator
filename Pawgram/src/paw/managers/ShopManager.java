2eaef5ba736c8c248b94afcb621348adb38ced7b
Sushi Roll <gabb09.perez@gmail.com>
2025-11-11 20:04:06 +0800

    Delete Pawgram directory

--- Pawgram/src/paw/managers/ShopManager.java
+++
@@ -1,9 +0,0 @@
-package paw.managers;
-
-import paw.models.*;
-import java.util.*;
-
-public class ShopManager {
-    private List<Item> items = new ArrayList<>();
-    private Scanner input = new Scanner(System.in);
-}
